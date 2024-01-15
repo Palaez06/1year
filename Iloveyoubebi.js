9c408eba1506d67bfc437bcd78d84d3b9c1b8b38
Palaez06 <156689438+Palaez06@users.noreply.github.com>
2024-01-15 23:37:21 +0800

    Add files via upload

---
+++ Iloveyoubebi.js
@@ -0,0 +1,3 @@
+document.getElementById("shirtlessBtn").onclick = () => window.location.assign("shirtless.html");
+
+document.getElementById("presentBtn").onclick = () => window.location.assign("song.html");
