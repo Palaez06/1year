56a3568f4fefd6fb2af769f2969eb1dc82439db5
Palaez06 <156689438+Palaez06@users.noreply.github.com>
2024-01-15 23:25:49 +0800

    Add files via upload

---
+++ backup2.js
@@ -0,0 +1,8 @@
+let givenname;
+
+document.getElementById("submitBtn").onclick = function(){
+    givenname = document.getElementById("textbox1").value.toLowerCase();
+    let name = givenname == "monkey" ? window.location.assign("backup3.html") :
+    "you entered the wrong password";
+    document.getElementById("h2").textContent = name;
+}
