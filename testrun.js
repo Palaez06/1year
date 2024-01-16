16386c1b8f574623ef8584f77e2e50ea86bf8f6f
Palaez06 <156689438+Palaez06@users.noreply.github.com>
2024-01-16 16:53:18 +0800

    Update testrun.js

--- testrun.js
+++ testrun.js
@@ -2,7 +2,7 @@ let givenpassword;
 
 document.getElementById("button1").onclick = function (){
     givenpassword = document.getElementById("textbox1").value;
-    let  password = givenpassword == -0.961261695 ? window.location.assign("backup.html") : 
+    let  password = givenpassword == -0.961 ? window.location.assign("backup.html") : 
     "You've entered the wrong password, please try again.";
     document.getElementById("myh2").textContent = password;
 }
