1f0faa2b534c262dfe22bb51c6c8d5c95d71a801
Palaez06 <156689438+Palaez06@users.noreply.github.com>
2024-01-20 23:02:49 +0800

    Delete backup.js

--- backup.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("submitBtn").onclick = function () {
-    let givennick = document.getElementById("textbox1").value.toLowerCase();
-    let nick = givennick == "washi" ? window.location.assign("backup2.html"):
-    document.getElementById("h2").textContent = "You entered the wrong password";
-}
