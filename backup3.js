9f96f3dc794800a05c7ec5ae88581ca340f89fdf
Palaez06 <156689438+Palaez06@users.noreply.github.com>
2024-01-20 23:03:10 +0800

    Delete backup3.js

--- backup3.js
+++
@@ -1,8 +0,0 @@
-let givensubject;
-
-document.getElementById("submitBtn").onclick = function (){
-    givensubject = document.getElementById("textbox1").value.toLowerCase();
-    let subject = givensubject == "physics"? window.location.assign("Happy1year.html") : 
-    "that's wrong";
-    document.getElementById("h2").textContent = subject;
-}
