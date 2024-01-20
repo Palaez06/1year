2d307ce87f0ca2c79ec7fd1411cd6262ad6ea988
Palaez06 <156689438+Palaez06@users.noreply.github.com>
2024-01-20 23:03:00 +0800

    Delete backup2.js

--- backup2.js
+++
@@ -1,8 +0,0 @@
-let givenname;
-
-document.getElementById("submitBtn").onclick = function(){
-    givenname = document.getElementById("textbox1").value.toLowerCase();
-    let name = givenname == "monkey" ? window.location.assign("backup3.html") :
-    "you entered the wrong password";
-    document.getElementById("h2").textContent = name;
-}
